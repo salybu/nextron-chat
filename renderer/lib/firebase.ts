5f9d5ab45038d7993177d8695942bd2fd83fcf04
salybu <rkdmsdl93@gmail.com>
2021-12-26 22:19:45 +0900

    feat: install dotenv, use process.env variables

--- renderer/lib/firebase.ts
+++ renderer/lib/firebase.ts
@@ -1,9 +1,19 @@
+import dotenv from 'dotenv';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
 import * as admin from 'firebase-admin';
 
-const firebaseConfig = {};
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
@@ -13,12 +23,16 @@ if (!firebase.apps.length) {
 const auth = firebase.auth();
 
 // firebase-admin
-const firebasePrivateKey = 'xxx';
+const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({}),
-    databaseURL: 'xxx',
+    credential: admin.credential.cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   });
 }
 
