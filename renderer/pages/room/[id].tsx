b2f847a699415534fa06b158076360aa80ffbb62
salybu <rkdmsdl93@gmail.com>
2021-12-28 12:27:55 +0900

    feat: add scroll down feature, conditional render

--- renderer/pages/room/[id].tsx
+++ renderer/pages/room/[id].tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
 import useAuth from '../../lib/context/auth';
 import useChat from '../../components/chat/useChat';
 import ChatMessage from '../../components/chat/ChatMessage';
@@ -12,16 +12,23 @@ const Room: NextPage = () => {
   const {
     query: { id },
   } = useRouter();
+
   const { loggedUser } = useAuth();
   const { messages, getAllMessage, submitMessage } = useChat();
 
   const [input, setInput] = useState<string>();
+
+  const bottom = useRef<HTMLSpanElement>();
 
   useEffect(() => {
     (async () => {
       await getAllMessage(id as string);
     })();
   }, []);
+
+  useEffect(() => {
+    bottom?.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
 
   const sendMessage = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -35,11 +42,14 @@ const Room: NextPage = () => {
         <title>Nextron Chat - Chatting Room with {id}</title>
       </Head>
       <main>
-        {messages?.map((message) => (
-          <ChatMessage message={message} isSent={message.sentBy.id === loggedUser.id}></ChatMessage>
-        ))}
+        {messages?.length == 0 ? (
+          <h2 style={{ textAlign: 'center' }}>no message here</h2>
+        ) : (
+          messages?.map((message, idx) => <ChatMessage key={idx} message={message} isSent={message.sentBy.id === loggedUser.id}></ChatMessage>)
+        )}
       </main>
       <ChatInput input={input} setInput={setInput} sendMessage={sendMessage} />
+      <span ref={bottom}></span>
     </>
   );
 };
